dfd01a67adcb52f984bc3a9755a312cc60fc5a82
Egor Tensin <Egor.Tensin@gmail.com>
2023-08-04 15:40:30 +0200

    digitalocean/firewall: open ports for UDP also

--- digitalocean/firewall/main.tf
+++ digitalocean/firewall/main.tf
@@ -21,6 +21,16 @@ resource "digitalocean_firewall" "this" {
     }
   }
 
+  dynamic "inbound_rule" {
+    for_each = var.open_ports
+
+    content {
+      protocol         = "udp"
+      port_range       = inbound_rule.value
+      source_addresses = ["0.0.0.0/0", "::/0"]
+    }
+  }
+
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
