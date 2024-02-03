7ce1459f4a00805851fe36e87a489749e5f323a8
Egor Tensin <egor@tensin.name>
2024-02-03 22:47:58 +0100

    digitalocean/server: customizable alerts w/ higher thresholds

--- digitalocean/server/alerts.tf
+++ digitalocean/server/alerts.tf
@@ -5,24 +5,30 @@ resource "digitalocean_monitor_alert" "cpu" {
   alerts {
     email = [data.digitalocean_account.this.email]
   }
+
   description = "CPU utilization on ${digitalocean_droplet.this.name}"
   window      = "5m"
   type        = "v1/insights/droplet/cpu"
   compare     = "GreaterThan"
-  value       = 70
+  value       = var.alert_cpu_threshold
   enabled     = true
   entities    = [digitalocean_droplet.this.id]
+
+  count = var.alert_cpu_enable ? 1 : 0
 }
 
-resource "digitalocean_monitor_alert" "load1" {
+resource "digitalocean_monitor_alert" "loadavg1" {
   alerts {
     email = [data.digitalocean_account.this.email]
   }
+
   description = "1-min load avg on ${digitalocean_droplet.this.name}"
   window      = "5m"
   type        = "v1/insights/droplet/load_1"
   compare     = "GreaterThan"
-  value       = 5
+  value       = var.alert_loadavg1_threshold
   enabled     = true
   entities    = [digitalocean_droplet.this.id]
+
+  count = var.alert_loadavg1_enable ? 1 : 0
 }
--- digitalocean/server/variables.tf
+++ digitalocean/server/variables.tf
@@ -38,3 +38,20 @@ variable "size" {
   type    = string
   default = "s-1vcpu-2gb"
 }
+
+variable "alert_cpu_enable" {
+  type    = bool
+  default = true
+}
+variable "alert_cpu_threshold" {
+  type    = number
+  default = 80
+}
+variable "alert_loadavg1_enable" {
+  type    = bool
+  default = true
+}
+variable "alert_loadavg1_threshold" {
+  type    = number
+  default = 5
+}
