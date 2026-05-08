2fedd3442e094dd44b44663a71c95edc9c52b5ad
Egor Tensin <egor@tensin.name>
2026-05-08 18:35:57 +0200

    aws: DynamoDB locks are deprecated

--- aws/state/main.tf
+++ aws/state/main.tf
@@ -31,14 +31,3 @@ resource "aws_s3_bucket_public_access_block" "default" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_dynamodb_table" "locks" {
-  name         = var.dynamodb_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
--- aws/state/variables.tf
+++ aws/state/variables.tf
@@ -1,6 +1,3 @@
 variable "bucket_name" {
   type = string
 }
-variable "dynamodb_table_name" {
-  type = string
-}
